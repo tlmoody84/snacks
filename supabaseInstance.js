c7dd11470e019a1e9a7069a95e30e96dc129598e
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-08-31 17:29:31 -0400

    deleted old

--- supabaseInstance.js
+++ supabaseInstance.js
@@ -1,17 +1,3 @@
-// require("dotenv").config();
-// const { createClient } = require("@supabase/supabase-js");const axios = require("axios");
-// const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-// const instance = axios.create({
-//     baseURL: process.env.SUPABASE_URL + "/rest/v1",
-//     timeout: 1000,
-//     headers: { 
-//       apikey: process.env.SUPABASE_KEY, 
-//       Authorization: "Bearer" + process.env.SUPABASE_KEY },
-    
-//     });
-
-//   module.exports = instance, supabase;
-
 const axios = require('axios')
 require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
